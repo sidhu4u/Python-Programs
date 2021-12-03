5bc1680b24a8cfefc269a0e45f74f807dba27899
Sidhu4u <sidhu.dudu@gmail.com>
2021-12-03 19:02:58 +0530

    updated list_basic.py
    Added pgm5 in list_basic.py file

--- List_Programs/list_basic.py
+++ List_Programs/list_basic.py
@@ -1,13 +1,17 @@
 # 1. Python program to find length of list
 # 2. Python program to check if element exists in list
 # 3. Python program to find sum of elements in list
+# 4. Python program to Multiply all numbers in the list
+# 5. Python program to print even and odd numbers in a list
 
 
-from list_functions import list_len, ele_exist_list, sum_ele
+from list_functions import list_ele, number_list, list_len, ele_exist_list, sum_ele, mul_ele, even_odd_list
 
 print("1.Python program to find length of list\n\
 2.Python program to check if element exists in list\n\
-3.Python program to find sum of elements in list")
+3.Python program to find sum of elements in list\n\
+4.Python program to Multiply all numbers in the list\n\
+5.Python program to print even and odd numbers in a list")
 
 
 list = []
@@ -15,24 +19,12 @@ try:
     choice = int(input("Choice : "))
     if choice == 1:
         # 1.Python program to find length of list
-        while True:
-            n = input("Element(Press 'n' to Quit) = ")
-            if n == 'N' or n == 'n':
-                break
-            list.append(n)
-        print("Given List = ", list)
-
-        print("Length of given List = ", list_len(list))
+        print("Length of given List = ", list_len(list_ele()))
 
     elif choice == 2:
         # 2.Python program to check if element exists in list
-        while True:
-            n = input("Element(Press 'n' to Quit) = ")
-            if n == 'N' or n == 'n':
-                break
-            list.append(n)
-        print("Given List = ", list)
-        ele = input("Element = ")
+        list = list_ele()
+        ele = input("Search element = ")
 
         b = ele_exist_list(list, ele)
         if b:
@@ -42,20 +34,17 @@ try:
 
     elif choice == 3:
         # 3.Python program to find sum of elements in list
-        sum = 0
-        n = 0
-        while True:
-            try:
-                n = int(input("Element(Press '0' to Quit) = "))
-            except ValueError:
-                print("please enter number to conti...")
-            else:
-                if n == 0:
-                    break
-                list.append(n)
+        print("sum = ", sum_ele(number_list()))
 
-        print("\nGiven List = ", list)
-        print("sum = ", sum_ele(list))
+    elif choice == 4:
+        # 4.Python program to Multiply all numbers in the list
+        print("Multiplication = ", mul_ele(number_list()))
+
+    elif choice == 5:
+        # 5.Python program to print even and odd numbers in a list
+        res = even_odd_list(number_list())
+        print("Even numbers in list = ", res[0])
+        print("Odd numbers in list = ", res[1])
 
     else:
         print("Wrong Choice!")
--- List_Programs/list_functions.py
+++ List_Programs/list_functions.py
@@ -1,3 +1,31 @@
+
+def list_ele():
+    list = []
+    while True:
+        n = input("Element(Press 'n' to Quit) = ")
+        if n == 'N' or n == 'n':
+            break
+        list.append(n)
+    print("Given List = ", list)
+    return list
+
+
+def number_list():
+    n = 0
+    list = []
+    while True:
+        try:
+            n = int(input("Element(Press '0' to Quit) = "))
+        except ValueError:
+            print("please enter number to conti...")
+        else:
+            if n == 0:
+                break
+            list.append(n)
+    print("\nGiven List = ", list)
+    return list
+
+
 def list_len(list):
     return len(list)
 
@@ -17,3 +45,21 @@ def sum_ele(list):
     for x in list:
         sum += int(x)
     return sum
+
+
+def mul_ele(list):
+    mul = 1
+    for x in list:
+        mul *= int(x)
+    return mul
+
+
+def even_odd_list(list):
+    e = []
+    o = []
+    for i in list:
+        if i % 2 == 0:
+            e.append(i)
+        else:
+            o.append(i)
+    return e, o
